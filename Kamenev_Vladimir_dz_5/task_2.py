8a8128305ab13be7bedd3b7715d259f87ffb608b
Vladimir <kvv23@tut.by>
2021-02-28 21:54:13 +0300

    Домашнее задание по уроку 5. Задачи №2-5

---
+++ Kamenev_Vladimir_dz_5/task_2.py
@@ -0,0 +1,15 @@
+def odd_nums(quantity):
+    gen = (i for i in range(1, quantity + 1, 2))
+    return gen
+
+
+odd_to_15 = odd_nums(15)
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+next(odd_to_15)
